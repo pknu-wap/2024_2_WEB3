edcd0201329e9f0dab27fe02df4e85fa7d9fd6ce
yeeeon02 <yeonn128902@naver.com>
2024-10-04 01:47:48 +0900

    feat: 기본 라우팅 설정
    페이지 경로 임시 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/detail1" element={""} />
+          <Route path="/detail2" element={""} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
