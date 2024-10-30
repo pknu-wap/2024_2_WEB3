74d9c41a7ca777d2833863b0b5e42d0629252851
유하경 <hagyeong1129@naver.com>
2024-10-30 19:24:46 +0900

    회원가입 완료

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import FruitWinePage from "./pages/fruitWinePage";
 import SignInPage from "./pages/SignInPage";
 import Mypage from "./pages/mypage";
 import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/cheongtakju" element={<CheongtakjuPage />} />
-          <Route path="/fruitWine" element={<FruitWinePage />} />
+          <Route path="/cheongtakju" element={<CheongtakjuPage />} />{" "}
+          <Route path="/fruitWine" element={<FruitWinePage />} />{" "}
           <Route path="/signIn" element={<SignInPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
           <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </BrowserRouter>
