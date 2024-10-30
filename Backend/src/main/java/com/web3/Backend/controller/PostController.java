0cd8e3940d95edd23e8a7b965951aabf0ab85990
Ksuwon <su348192@gmail.com>
2024-10-30 21:24:24 +0900

    Remove sensitive information from application.properties

--- Backend/src/main/java/com/web3/Backend/controller/PostController.java
+++ Backend/src/main/java/com/web3/Backend/controller/PostController.java
@@ -6,6 +6,7 @@ import com.web3.Backend.dto.PostDto;
 import com.web3.Backend.security.CurrentUser;
 import com.web3.Backend.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,6 +40,66 @@ public class PostController {
 
         Response response = new Response("200", result);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //청탁주 페이지
+    @GetMapping("/post/cheongtakju/{page}")
+    public ResponseEntity<Response> getCheongTakjuPage(
+            @PathVariable int page,
+            @RequestParam(defaultValue="10") int size){
+
+        Page<PostDto> postPage = postService.getCheongTakjuPage(page,size);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("content",postPage.getContent());
+        data.put("totalPages",postPage.getTotalPages());
+        data.put("currentPage",postPage.getNumber());
+
+        Response response = new Response("200","청탁주 정보 조회 성공",data);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //과실주 페이지
+    @GetMapping("/post/fruitWine/{page}")
+    public ResponseEntity<Response> getFruitWinePage(
+            @PathVariable int page,
+            @RequestParam(defaultValue="10") int size){
+
+        Page<PostDto> postPage = postService.getFruitWinePage(page,size);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("content",postPage.getContent());
+        data.put("totalPages",postPage.getTotalPages());
+        data.put("currentPage",postPage.getNumber());
+
+        Response response = new Response("200","과실주 정보 조회 성공",data);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    //검색 기능
+    @GetMapping("/post/search")
+    public ResponseEntity<Response> searchPost(
+            @RequestParam(value="drinkName",required = false) String drinkName,
+            @RequestParam(value="page",defaultValue="0") int page,
+            @RequestParam(value="size",defaultValue="10") int size) {
+
+        Map<String, Object> data = new HashMap<>();
+
+        if (drinkName == null || drinkName.trim().isEmpty()) {
+            //빈 문자열
+            Response response = new Response("400", "잘못된 접근", data);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        Page<PostDto> searchResult = postService.searchPostByName(drinkName, page, size);
+        if (searchResult.isEmpty()) {
+            Response response = new Response("404", "해당 데이터가 존재하지 않습니다.", data);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        } else {
+            data.put("searchResult", searchResult);
+            Response response = new Response("200", "검색 결과 조회 성공", data);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
     }
 
 }
