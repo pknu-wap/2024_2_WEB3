a9a6224bbbf5f9ee0d1522d1777b53da892d6105
Ksuwon <su348192@gmail.com>
2024-11-13 05:06:31 +0900

    reissue 경로 허용

--- Backend/src/main/java/com/web3/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/web3/Backend/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/post/cheongtakju/**").permitAll()
                         .requestMatchers("/api/post/fruitWine/**").permitAll()
                         .requestMatchers("/api/post/search").permitAll()
-                        .requestMatchers("/reissue").permitAll()
+                        .requestMatchers("/auth/reissue").permitAll()
                         .requestMatchers("/auth/signup","/auth/login","/auth/logout","/error").permitAll()
                         .requestMatchers("/images/**").permitAll()
                         .anyRequest().authenticated() // 그 외 경로는 인증 필요
