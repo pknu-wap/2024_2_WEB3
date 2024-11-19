b34f9b4b61b7d0518eff7ca8b8770e0bd84a8710
yeeeon02 <yeonn128902@naver.com>
2024-11-19 22:23:22 +0900

    feat:메인 페이지 수정
    - 이미지 추가
    - 링크 연결
    - 카테고리 소개 글 추가
    - 메인컨텐츠 css 수정

--- client/src/components/mainContents/MainContents.js
+++ client/src/components/mainContents/MainContents.js
@@ -8,21 +8,109 @@ const MainContents = () => {
   return (
     <div className="MainContents">
       <div className="banner-section">
-        <img src="/images/banner-img-figma.png" className="background-img" />
+        <img
+          src="images/mainpage/banner-img-figma.png"
+          className="banner-img"
+        />
       </div>
 
-      <div className="contents-section">
-        <p>aefaefasfef</p>
-        <p>aefaefasfef</p>
-        <p>aefaefasfef</p>
-        <p>aefaefasfef</p>
+      {/* 검색창 */}
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="원하는 술을 검색해보세요!"
+      />
+
+      {/* 카테고리 섹션 */}
+      <div className="category-section">
+        <p className="category-title">
+          다채로운 전통주 한 잔, <span className="highlight">홀짝</span> 맛보는
+          즐거움
+        </p>
+
+        {/* 첫 번째 블록 */}
+        <div class="category-block category-block1">
+          <div class="cate-img cate-img1">
+            <img src="images/mainpage/category-img1.png" alt="category-img1" />
+          </div>
+          <div class="cate-text cate-text1">
+            <h3>
+              쌀과 물로 빚어낸 맑은 술, <span className="cate-span">청주</span>
+            </h3>
+            <p>
+              빛깔이 맑고 투명해 '맑은 술'로 불리는 청주는 쌀을 발효해 <br />
+              은은한 향과 부드러운 맛을 자랑합니다.
+            </p>
+            <Link to="/cheongtakju" className="cate-link">
+              더 알아보기 &gt;
+            </Link>
+          </div>
+        </div>
+        <img
+          src="/images/mainpage/sub-category-img1.png"
+          alt="sub-category-img1"
+          class="cate-sub cate-sub1"
+        />
+
+        {/* 두 번째 블록 */}
+        <div class="category-block category-block2">
+          <div class="cate-text cate-text2">
+            <h3>
+              부드럽고 진한 맛, 전통의 <span className="cate-span">탁주</span>
+            </h3>
+            <p>
+              쌀과 누룩으로 만든 걸쭉한 전통주인 탁주는 고소하고 깊은 맛이
+              특징입니다. <br />
+              신선한 발효 향이 어우러져 풍미가 깊습니다.
+            </p>
+            <Link to="/cheongtakju" className="cate-link">
+              더 알아보기 &gt;
+            </Link>
+          </div>
+          <div class="cate-img cate-img2">
+            <img src="images/mainpage/category-img2.png" alt="category-img2" />
+          </div>
+        </div>
+
+        {/* 세 번째 블록 */}
+        <div class="category-block category-block3">
+          <div class="cate-img cate-img3">
+            <img src="images/mainpage/category-img3.png" alt="category-img3" />
+          </div>
+          <div class="cate-text cate-text3">
+            <h3>
+              과일의 향과 맛을 담은 술,
+              <span className="cate-span">과실주</span>
+            </h3>
+            <p>
+              매실, 복숭아, 오미자 등 다양한 과일로 만든 전통주인 과실주는
+              과일의 상큼한 향과 <br />
+              맛을 즐길 수 있습니다.
+            </p>
+            <Link to="/fruitWine" className="cate-link">
+              더 알아보기 &gt;
+            </Link>
+          </div>
+        </div>
+
+        <div class="cate-sub-images">
+          <img
+            src="/images/mainpage/sub-category-img1.png"
+            alt="sub-category-img1"
+            class="cate-sub cate-sub1"
+          />
+          <img
+            src="/images/mainpage/sub-category-img2.png"
+            alt="sub-category-img2"
+            class="cate-sub cate-sub1"
+          />
+          <img
+            src="/images/mainpage/sub-category-img3.png"
+            alt="sub-category-img3"
+            class="cate-sub cate-sub1"
+          />
+        </div>
       </div>
-
-      {/* <div className="category-section"></div>
-      <div className="bookmark-section"></div>
-      <div className="pairing-section"></div> */}
-
-      {/* footer 여기 추가? */}
     </div>
   );
 };
