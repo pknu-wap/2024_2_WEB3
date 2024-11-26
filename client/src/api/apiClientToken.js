7af123471c0d9e700ba91147932178e9ea0d9585
유하경 <hagyeong1129@naver.com>
2024-11-26 22:11:50 +0900

    토큰 있는 헤더 api
    ㅎㅎ

---
+++ client/src/api/apiClientToken.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Axios 인스턴스 생성
+const apiClientToken = axios.create({
+  baseURL: process.env.REACT_APP_API_ROUTE, // .env에서 설정한 baseURL 사용
+
+});
+
+export default apiClientToken;
