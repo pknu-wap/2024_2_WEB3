3847b041251c373456f32e8eb950c9fa039246a3
yeeeon02 <yeonn128902@naver.com>
2024-11-29 06:42:59 +0900

    Update alcoholListApi.js

--- client/src/api/alcoholListApi.js
+++ client/src/api/alcoholListApi.js
@@ -24,11 +24,9 @@ const alcoholListApi = async (category, page, filters = {}) => {
     // console.log("서버 응답 메시지:", response.data.message);
     return response.data.data; // 서버에서 필요한 데이터 반환
   } catch (error) {
-    // 서버 응답이 있을 경우 처리
     if (error.response && error.response.data && error.response.data.message) {
       // console.log(`서버 에러 메시지: ${error.response.data.message}`);
     } else {
-      // 서버 응답이 없거나 다른 종류의 에러일 경우 기본 처리
       console.log(`API 요청 실패: ${error.message}`);
       alert(`API 요청 실패: ${error.message}`);
     }
