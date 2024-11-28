33bce1e9aa4f8a035e0ef4c7e9185a2999de20d1
StarrySnowy <kseun0349@gmail.com>
2024-11-28 09:15:21 +0100

    fix: searchresult

--- client/src/pages/searchresult.js
+++ client/src/pages/searchresult.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import AlcoholList from "../components/alcoholList/AlcoholList"; // AlcoholList 컴포넌트를 import
 
 const SearchResult = ({ category, filters }) => {
+  const location = useLocation();
   const [searchQuery, setSearchQuery] = useState(""); // 검색어 상태 관리
 
-  // 검색어 변경 시 호출되는 함수
+  // 쿼리 파라미터에서 검색어 가져오기
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const query = queryParams.get("query");
+    if (query) {
+      setSearchQuery(query); // 검색어 상태 업데이트
+    }
+  }, [location.search]);
+
+  // 검색어 변경 시 호출되는 함수 (추가 입력을 처리할 경우)
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
